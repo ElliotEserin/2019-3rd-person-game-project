c68386bc684d4964b96fee919ec29475eb2b7a2a
ElliotE <ellioteserin@gmail.com>
2019-11-03 18:18:29 +0000

    Adding foliage assets and (broken) Diamond assets

---
+++ 2019 game project/Assets/Scripts/diamondSpin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diamondSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject diamond;
+    public Vector3 rotation;
+
+    void Start()
+    {
+        rotation = new Vector3(0, 2f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        diamond.transform.Rotate(rotation, Space.Self);
+    }
+}
