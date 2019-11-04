7f958e02fb95e8be20aad42bf77903c0ae19f7fa
ElliotEserin <ellioteserin@users.noreply.github.com>
2019-11-04 16:30:10 +0000

    adding to asthetics

--- 2019 game project/Assets/Scripts/respawnSystem/setRespawn.cs
+++ 2019 game project/Assets/Scripts/respawnSystem/setRespawn.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SetRespawn : MonoBehaviour
 {
 
+    public GameObject diamond;
+
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<RespawnSystem>().currentRespawnLocation = transform;
+        diamond.GetComponent<diamondSpin>().rotation = new Vector3(0.25f,0.5f,0.25f);
 
+        diamond.GetComponent<Light>().intensity = 0;
+        diamond.GetComponent<Light>().color = new Color(1,1,1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        diamond.AddComponent<Rigidbody>();
+        diamond.GetComponent<diamondSpin>().enabled = false;
+        diamond.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
     }
 
 }
